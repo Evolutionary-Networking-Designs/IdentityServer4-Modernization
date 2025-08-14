c68ec35dfc47759987842cd45db824b76c1cde9b
Kiera Meredith <kiera@meredithkm.info>
2025-08-14 00:53:35 -0400

    Update root build.sh script

--- build.sh
+++ build.sh
@@ -6,22 +6,22 @@ mkdir nuget
 
 dotnet tool restore
 
-pushd ./src/Storage
-./build.sh "$@"
-popd
+# pushd ./src/Storage
+# ./build.sh "$@"
+# popd
 
 pushd ./src/IdentityServer4
 ./build.sh "$@"
 popd
 
-pushd ./src/EntityFramework.Storage
-./build.sh "$@"
-popd
+# pushd ./src/EntityFramework.Storage
+# ./build.sh "$@"
+# popd
 
-pushd ./src/EntityFramework
-./build.sh "$@"
-popd
+# pushd ./src/EntityFramework
+# ./build.sh "$@"
+# popd
 
-pushd ./src/AspNetIdentity
-./build.sh "$@"
-popd
+# pushd ./src/AspNetIdentity
+# ./build.sh "$@"
+# popd
